642737837bfc8b747a3d7900e949de8426eec4c4
Bestonfranklin <110010423+Bestonfranklin@users.noreply.github.com>
2022-09-20 11:42:50 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ def display_hash(hashtable) -> None:
 	for i in range(len(hashtable)):
 		print(i, ends=" ")
 		for j in hashtable[i]:
-			print("-->,j, end=" ")
+			print("-->",j, end=" ")
 		print()	      
 
 
